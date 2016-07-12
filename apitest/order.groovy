8423b2ce659a29f00a2417c3ed6e376c5e5a747c
nahongxu <nahongxu@sogou-inc.com>
2016-07-12 18:56:42 +0800

    add apitest

---
+++ apitest/order.groovy
@@ -0,0 +1,19 @@
+import static lib.BDD.*;
+
+def phoneUnicom="18600539639"
+CONFIG(
+  server: "http://127.0.0.1",
+  headers: [host: "fee.pay.sogou.com"]
+)
+
+
+
+def phoneInfo
+GET("/api/phoneinfo/phones=$phoneUnicom")
+EXPECT {
+  json.code = 0
+  json.'data' = NotEmpty
+  json.'data' = Grep {
+    obj.operator= "CHINA_UNICOM"
+  }
+}
