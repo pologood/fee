a1145197729c989f8b8fa907e8151b8e91166176
nahongxu <nahongxu@sogou-inc.com>
2016-07-13 14:28:08 +0800

    modify query param

--- apitest/order.groovy
+++ apitest/order.groovy
@@ -1,14 +1,16 @@
 import static lib.BDD.*;
 
 def phoneUnicom="18600539639"
+
 CONFIG(
   server: "http://127.0.0.1",
   headers: [host: "test.fee.pay.sogou.com"]
 )
 
-GET("/api/phoneinfo?"){
-  r.query = [phones: $phoneUnicom]
+GET("/api/phoneinfo"){
+  r.query = [phones: phoneUnicom]
 }
+
 EXPECT {
   json.code = 0
   json.'data' = NotEmpty
